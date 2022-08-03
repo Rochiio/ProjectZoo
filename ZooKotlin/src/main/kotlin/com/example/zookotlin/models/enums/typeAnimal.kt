1d8ebc8f2e40223f0524ea31c73dec09c308d492
Rocio <rpalaofernandez@gmail.com>
2022-08-03 10:08:36 +0200

    repo animales

---
+++ ZooKotlin/src/main/kotlin/com/example/zookotlin/models/enums/typeAnimal.kt
@@ -0,0 +1,11 @@
+package com.example.zookotlin.models.enums
+
+/**
+ * Tipos de animales que hay en el zoológico.
+ */
+enum class typeAnimal(value: String) {
+    MAMIFEROS("MAMIFEROS"),
+    AVES("AVES"),
+    REPTILES("REPTILES"),
+    PECES("PECES")
+}
---
+++ ZooKotlin/src/main/kotlin/com/example/zookotlin/repositories/animals/AnimalsRepository.kt
@@ -0,0 +1,11 @@
+package com.example.zookotlin.repositories.animals
+
+import com.example.zookotlin.models.Animal
+import com.example.zookotlin.repositories.ICRUD
+import javafx.collections.ObservableList
+
+interface AnimalsRepository: ICRUD<Animal> {
+    fun findAll(): ObservableList<Animal>
+    fun findById(id: Int): Animal
+    fun modifyById(id: Int, newAnimal: Animal):Animal
+}
